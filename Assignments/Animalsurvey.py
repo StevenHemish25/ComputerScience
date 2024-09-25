2eeb8d04319c92a3fccaa6fcdcbdb808f5923640
StevenHemish25 <818432@mystma.org>
2024-09-25 15:09:31 -0500

    Animal Survey w/comments

--- Assignments/Animalsurvey.py
+++ Assignments/Animalsurvey.py
@@ -1,13 +1,14 @@
-favorite_animal = (input("What is your favorite animal?\n>"))
-print("You answered: " + favorite_animal)
-why = (input("Why is this your favorite?\n>"))
+favorite_animal = (input("What is your favorite animal?\n>")) #User answers animal
+print("You answered: " + favorite_animal)  #writes "you answered xyz"
+why = (input("Why is this your favorite?\n>")) #user answers why
 print("You answered: " + why)
-sound = (input("What sound does the animal make?\n>"))
+sound = (input("What sound does the animal make?\n>")) #user answers sound
 print("You answered: " + sound)
-food = (input("What do they eat? \n>"))
+food = (input("What do they eat? \n>")) #user answers animal eats
 print("You answered: " + food)
-location = (input("Where do they live?\n>"))
+location = (input("Where do they live?\n>")) #user answers where
 print("You said they live: " + location)
-color = (input("What color are they?\n>"))
+color = (input("What color are they?\n>")) #User answers color
 print("They are: " + color)
 print("You favorite animal was " + favorite_animal + "\nThey are your favorite because " + why + "\nThey go " + sound + "\nThey eat " + food + "\nThey live " + location + "\nThey are " + color)
+#Final line prints all answers 
