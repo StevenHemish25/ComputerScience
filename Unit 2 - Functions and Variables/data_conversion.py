23b65316de3c9c58c47ea21fd4dad166dd5e09ee
StevenHemish25 <818432@mystma.org>
2024-09-25 15:19:07 -0500

    Data Conversion activity

--- Unit 2 - Functions and Variables/data_conversion.py
+++ Unit 2 - Functions and Variables/data_conversion.py
@@ -16,3 +16,14 @@ j = 10.0        #float -> int
 k = 254         #int -> byte
 
 a_str = str(a)
+b_str = str(b)
+c_int = int(c)
+d_bool = bool(d)
+e_str = str(e)
+f_str = str(f)
+g_int = int(g)
+h_flo = float(h)
+i_flo = float(i)
+j_int = int(j)
+k_byt = bin(k)
+
