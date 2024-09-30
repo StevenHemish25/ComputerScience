df5d883970d02465b1b145f4a8db802def6aa889
StevenHemish25 <818432@mystma.org>
2024-09-30 14:25:49 -0500

    Top 5 movies

---
+++ Unit 2 - Functions and Variables/functions.py
@@ -0,0 +1,36 @@
+def my_function():         #define the function with a name
+    print("Hello World!")
+    print("catdog")
+          
+print("Osowski r00lz d00d:")
+my_function()
+
+def top_movies(a, b, c, d, e):
+    print("1:Waterloo 1970")
+    print("2:Cars 2")
+    print("3:Airplane 1980")
+    print("4:Up!")
+    print("5:Wall-E")
+
+def add(x, y):
+    print(x + y) #x and y make function work with anything
+
+add(5, 10)
+add(2, 2)
+add(15, 15)
+add("cat", "dog")
+
+def top_five_movies(a, b, c, d, e):
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(e)
+
+
+
+
+top_five_movies("b","s","g","sdshg","qwyu")
+
+
+
